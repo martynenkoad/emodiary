c28f1b8fc7aa88830b1d51b20fe27710682fc5b2
martynenkoad <anastasiainenko@gmail.com>
2022-08-11 14:36:09 +0300

    commit package.json changes

--- backend/index.js
+++ backend/index.js
@@ -33,17 +33,30 @@ app.use('/api/user', userRoutes)
 app.use('/api/note', noteRoutes)
 
 // connection string
-mongoURI = 'mongodb+srv://nastya:eIudKM89AJO51exB@cluster0.we8xbbc.mongodb.net/?retryWrites=true&w=majority'
+//mongoURI = 'mongodb+srv://nastya:eIudKM89AJO51exB@cluster0.we8xbbc.mongodb.net/?retryWrites=true&w=majority'
+
+var mongoUrl = '"mongodb://localhost:27017"'
+    var mongoose = require('mongoose')
+    // updated 2021
+    mongoose.Promise = global.Promise;
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true)
+    
+    mongoose.connect(mongoUrl, { useUnifiedTopology: true })
+    .then(() => { log('Connected to MongoDB: %s \n ', mongoUrl) }) 
+    .catch((err) => { error('MongoDB connection error: %s \n', err); })
+
 
 // connect to db
-mongoose.connect(mongoURI)
-    .then(() => {
-        console.log('Connected to DB')
+// mongoose.connect(mongoURI)
+//     .then(() => {
+//         console.log('Connected to DB')
 
-        app.listen(4000, () => {
-            console.log(`Server listening on port ${process.env.PORT}....`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+//         app.listen(4000, () => {
+//             console.log(`Server listening on port ${process.env.PORT}....`)
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
