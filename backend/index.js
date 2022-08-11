06009bf87b97171c5f52e785d12372279712f17f
martynenkoad <anastasiainenko@gmail.com>
2022-08-11 14:18:48 +0300

    commit package.json changes

--- backend/index.js
+++ backend/index.js
@@ -33,17 +33,28 @@ app.use('/api/user', userRoutes)
 app.use('/api/note', noteRoutes)
 
 // connection string
-mongoURI = 'mongodb+srv://nastya:eIudKM89AJO51exB@cluster0.we8xbbc.mongodb.net/?retryWrites=true&w=majority'
+//mongoURI = 'mongodb+srv://nastya:eIudKM89AJO51exB@cluster0.we8xbbc.mongodb.net/?retryWrites=true&w=majority'
 
-// connect to db
-mongoose.connect(mongoURI)
-    .then(() => {
-        console.log('Connected to DB')
+mongoUrl = process.env.mongoURL
 
-        app.listen(4000, () => {
-            console.log(`Server listening on port ${process.env.PORT}....`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+mongoose.Promise = global.Promise;
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true)
+    
+    mongoose.connect(mongoUrl, { useUnifiedTopology: true })
+    .then(() => { log('Connected to MongoDB: %s \n ', mongoUrl) }) 
+    .catch((err) => { error('MongoDB connection error: %s \n', err); })
+
+// // connect to db
+// mongoose.connect(mongoURI)
+//     .then(() => {
+//         console.log('Connected to DB')
+
+//         app.listen(4000, () => {
+//             console.log(`Server listening on port ${process.env.PORT}....`)
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
